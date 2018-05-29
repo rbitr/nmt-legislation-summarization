256b06c618ca9d18acbbd0919a1bdf6c1b7b88b9
rbitr <35141631+rbitr@users.noreply.github.com>
2018-05-29 11:23:13 -0400

    Update extract_html_data.py

--- extract_html_data.py
+++ extract_html_data.py
@@ -20,7 +20,7 @@ for l in letters:
     #summaries = []
     #titles = []
     print (l)
-    soup_page = BeautifulSoup(requests.get("http://laws.justice.gc.ca/eng/regulations/"+l+".html").text,"html.parser")
+    soup_page = BeautifulSoup(requests.get("http://laws.justice.gc.ca/eng/acts/"+l+".html").text,"html.parser")
     aaa = [a for a in soup_page.find_all('a') if hasattr(a,'class')]
     links = [a['href'].split('/')[0] for a in soup_page.find_all('a') if  a.get_attribute_list('class')[0]=="TocTitle"]
     #links = [a for a in soup_page.find_all('a') if a.has_attribute
